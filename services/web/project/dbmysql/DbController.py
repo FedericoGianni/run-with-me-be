407af6f5c9c968334d24f8484bc92d33165725fb
FedericoGianni <federico.deicas@mail.polimi.it>
2022-03-18 14:46:55 +0100

    check if username already taken before registering new user

--- services/web/project/dbmysql/DbController.py
+++ services/web/project/dbmysql/DbController.py
@@ -313,6 +313,9 @@ class DbController():
                     i = insert(self.__bookingsTable)
                     i = i.values(booking)
                     newBookingId = self.session.execute(i).inserted_primary_key[0]
+                    # increase +1 on current_participants from this event
+                    self.session.query(self.__eventsTable).filter(self.__eventsTable.id == booking['event_id']).update({'current_participants': event.current_participants + 1})
+                    self.session.commit()
                 else:
                     newBookingId = result[0].id
                 
